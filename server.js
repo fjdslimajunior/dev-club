de992df74d2a835d64c7cd7e563d8cf9d2642bd8
Lima Junior <fjdlimajunior@gmail.com>
2024-06-24 14:25:00 -0300

    starting Node

---
+++ server.js
@@ -0,0 +1,26 @@
+
+
+import express from 'express';
+
+
+const app = express()
+app.use(express.json())
+
+const users = []
+
+app.post('/usuarios', (request, response) => {
+
+    users.push(request.body)
+
+    response.status(201).json(resquest.body)
+})
+
+app.get('/usuarios', (request, response) => {
+    response.status(200).json(users)
+})
+
+app.listen(3000);
+
+
+//Access
+// F9N4QXNvr0ouIgV5
